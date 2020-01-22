9eadf65b7dba071080da9e699310836ae286d4e0
maxkorzan <maxkorzan@gmail.com>
2020-01-22 13:23:34 -0500

    first commit, simple math display

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+
+        System.out.println("mynumber: " + mynumber);
+        System.out.println("myothernumber: " + myothernumber);
+
+    }
+}
